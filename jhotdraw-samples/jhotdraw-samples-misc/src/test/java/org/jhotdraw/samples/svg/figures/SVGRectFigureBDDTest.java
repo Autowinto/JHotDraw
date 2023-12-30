362e9b914205498964e05b567c35d2fd67b00c5a
jonfr <jojak19@student.sdu.dk>
2023-12-30 12:46:23 +0100

    removed unused imports

--- jhotdraw-samples/jhotdraw-samples-misc/src/test/java/org/jhotdraw/samples/svg/figures/SVGRectFigureBDDTest.java
+++ jhotdraw-samples/jhotdraw-samples-misc/src/test/java/org/jhotdraw/samples/svg/figures/SVGRectFigureBDDTest.java
@@ -3,9 +3,6 @@ package org.jhotdraw.samples.svg.figures;
 import com.tngtech.jgiven.junit.ScenarioTest;
 import org.junit.Test;
 
-import java.awt.*;
-import java.awt.image.BufferedImage;
-
 public class SVGRectFigureBDDTest extends ScenarioTest<
         givenSVGRectRectangleFigure,
         whenSVGRectFigureDrawing,
